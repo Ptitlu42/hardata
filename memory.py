a28915a75e08f08fe8ad481dd778cb575536f6d6
PtitLu42 <lucas.beyer@gmx.fr>
2023-05-30 18:34:25 +0200

    initial commit

--- memory.py
+++ memory.py
@@ -1,9 +1,9 @@
 import psutil
 
-def memory_data():
+def getMemoryData():
     memory_usage = psutil.virtual_memory().percent
     print (f"{memory_usage}")
     return memory_usage 
 
 
-memory_data()
+getMemoryData()
